a17687303f3e28b9b2b7114836548a874608ce6e
Marshall Pierce <marshall@mpierce.org>
2017-10-04 17:06:46 -0500

    Add basic cli example

--- README.md
+++ README.md
@@ -7,6 +7,41 @@ Stream VByte is a variable-length unsigned int encoding designed to make SIMD pr
 
 See https://lemire.me/blog/2017/09/27/stream-vbyte-breaking-new-speed-records-for-integer-compression/ and https://arxiv.org/pdf/1709.08990.pdf for details on the format. The reference C implementation is https://github.com/lemire/streamvbyte.
 
+# Play with the CLI example
+
+There's a `cli.rs` example provided that demonstrates encoding and decoding.
+
+To encode some numbers, provide numbers (one per line) to stdin, and the encoded result will be written to stdout.
+
+Example using `jot` to produce the numbes `1` to `100`: `jot 100 | cargo run --example cli enc | base64`
+
+Output, with cargo build output removed (the "Encoded ..." is on stderr for human convenience):
+
+```
+Encoded 100 numbers
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8g
+ISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZ
+WltcXV5fYGFiY2Q=
+```
+
+There's a corresponding decode mode that reads the encoded format on stdin and emits the contents, one number per line. Here, we encode some numbers then decode them again: `jot 10 | cargo run --example cli enc | cargo run --example cli dec 10`
+
+```
+Encoded 10 numbers
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Decoded 10 numbers
+
+```
+
 # Using SIMD
 
 There is one SIMD-accelerated `Decoder` implementation: `x86::Ssse3`, available when you enable the `x86_ssse3` feature for this crate. [SSSE3](https://en.wikipedia.org/wiki/SSSE3) has been around since Core-era Intel CPUs, so any modern `x86_64` system should have it. Unless you're writing a service for specific hardware that you know has the feature, you may need to do some runtime detection and decide at runtime whether or not to use the SSSE3 decoder. Something like [raw-cpuid](https://crates.io/crates/raw-cpuid) will probably be useful for that.
---
+++ examples/cli.rs
@@ -0,0 +1,60 @@
+extern crate stream_vbyte;
+
+use std::io::{BufRead, Read, Write};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <enc|dec> [count, if mode=dec]", args[0]);
+        std::process::exit(1);
+    }
+
+    // TODO use clap so arg parsing is less messy
+
+    match args[1].as_str() {
+        "enc" => {
+            let mut nums: Vec<u32> = Vec::new();
+
+            let stdin = std::io::stdin();
+            let stdin_handle = stdin.lock();
+
+            let nums: Vec<u32> = stdin_handle.lines()
+                    .map(|l| l.expect("Should be able to read stdin"))
+                    .map(|s| s.parse().expect("Each line must be a u32"))
+                    .collect();
+
+            let mut encoded = Vec::new();
+            encoded.resize(nums.len() * 5, 0);
+            let encoded_len = stream_vbyte::encode::<stream_vbyte::Scalar>(&nums, &mut encoded);
+
+            let stdout = std::io::stdout();
+            let mut stdout_handle = stdout.lock();
+            stdout_handle.write_all(&encoded[0..encoded_len]).expect("Should be able to write to stdout");
+
+            eprintln!("Encoded {} numbers", nums.len());
+        }
+        "dec" => {
+            let count: usize = args[2].parse().expect("Arg to 'dec' must be a number");
+            let stdin = std::io::stdin();
+            let mut stdin_handle = stdin.lock();
+
+            let mut encoded = Vec::new();
+            stdin_handle.read_to_end(&mut encoded);
+
+            let mut decoded = Vec::new();
+            decoded.resize(count, 0);
+            stream_vbyte::decode::<stream_vbyte::Scalar>(&encoded, count, &mut decoded);
+
+            for d in &decoded {
+                println!("{}", d);
+            }
+
+            eprintln!("Decoded {} numbers", decoded.len());
+        }
+        _ => {
+            eprintln!("Uknown mode");
+            std::process::exit(1);
+        }
+    }
+}
